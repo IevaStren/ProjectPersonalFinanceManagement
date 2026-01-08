aa29f4b28d60b9b0026ef9e1840417e434720146
IevaStren <116548267+IevaStren@users.noreply.github.com>
2026-01-08 22:10:03 +0200

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -42,9 +42,9 @@ def create_app():
     app.config["MAIL_SERVER"] = "smtp.gmail.com"
     app.config["MAIL_PORT"] = 587
     app.config["MAIL_USE_TLS"] = True
-    app.config["MAIL_USERNAME"] = os.environ.get("EMAIL")
-    app.config["MAIL_PASSWORD"] = os.environ.get("PASSWORD")
-    app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("EMAIL")
+    app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+    app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+    app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
 
 
     app.config['BABEL_DEFAULT_LOCALE'] = 'en'
